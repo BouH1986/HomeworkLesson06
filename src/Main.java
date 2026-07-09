e57009a5831ab42128871ef7ed2c73854e1af384
BouH1986 <jack-2006@mail.ru>
2026-07-09 07:37:31 +0400

    changed name

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        Author author01 = new Author("Evgen", "Elaev", 3);
+        Author author01 = new Author("Ivan", "Ivanov", 3);
         Author author02 = new Author("Andrew", "Tanenbaum", 10);
 
         Book book01 = new Book("Book01", 2025, author01, 30);
         Book book02 = new Book("Book02", 2026, author01, 50);
         Book book03 = new Book("Computer Networks", 2022, author02, 960);
 
-        toConsole("EvgeN", book01);
-        toConsole("Elaev", book02);
+        toConsole("IvaN", book01);
+        toConsole("Ivanov", book02);
         toConsole("Network", book03);
     }
 
@@ -17,6 +17,6 @@ public class Main {
         System.out.println();
         System.out.println("Это большая книга: " + book.isBig());
         System.out.println("Слово '" + word + "' в названии, имени или фамилии автора: " + book.matches(word));
-        System.out.println("Оценка книги: " + book.estimatePrice());
+        System.out.println("Оценка книги: " + book.estimatePrice()+" руб.");
     }
 }
